99986768fecce5bfff6851c3fb2e1e0add6d4200
ndineth100 <ndineth100@gmail.com>
2017-04-18 20:25:29 +0530

    18/04/2017 first commit

--- src/main/java/com/intelligentz/appointmentz/constants/RpiPinActions.java
+++
@@ -1,9 +0,0 @@
-package com.intelligentz.appointmentz.constants;
-
-/**
- * Created by lakshan on 11/12/16.
- */
-public class RpiPinActions {
-    public static final String ACTION_ON = "ON";
-    public static final String ACTION_OFF = "OFF";
-}
--- src/main/java/com/intelligentz/appointmentz/model/RequestMethod.java
+++
@@ -1,8 +0,0 @@
-package com.intelligentz.appointmentz.model;
-
-/**
- * Created by Malinda on 08/23/2015.
- */
-public enum RequestMethod {
-    POST,GET,DELETE,PUT
-}
